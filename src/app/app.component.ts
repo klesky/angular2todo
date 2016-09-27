deefe804b7dbd4002388a72792d1b5332a68e392
soonyoong@gmail.com <soonyoong@gmail.com>
2016-09-27 15:09:43 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
+
+const TODOS = [
+  new Todo("Todo 1"),
+  new Todo("Todo 2"),
+  new Todo("Todo 3")
+]
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Todo';
+  todos: Array<Todo>;
+  input = "";
+
+  constructor() {
+    this.todos = TODOS;
+  }
+
+  deleteTodo(index) {
+    this.todos.splice(index, 1);
+  }
+
+  addTodo(input){
+    this.todos.push(new Todo(input));
+    this.input ="";
+  }
+
 }
